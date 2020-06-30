66e219fc36fb0aeca75b5d281c157d87935e0f38
jdkfx <amateurranch@gmail.com>
2020-06-30 11:50:44 +0900

    update

--- main.c
+++ main.c
@@ -7,10 +7,12 @@
 #define BLOCK_WIDTH 4
 
 #define FIELD_HEIGHT 23
-#define FIELD_WIDTH 18
+#define FIELD_WIDTH 30
 
 void my_init_var(void);
 void my_init_var2(void);
+void my_init_board(void);
+void my_op(void);
 void my_make_block(void);
 void my_gameover(void);
 void my_get_key(void);
@@ -31,8 +33,11 @@ void my_draw_field(void);
 void my_clear_field(void);
 void my_fall_block(void);
 void my_timer(void);
+void my_change_lv(void);
 
+int next_block[BLOCK_HEIGHT][BLOCK_WIDTH];
 int block[BLOCK_HEIGHT][BLOCK_WIDTH];
+int hold_block[BLOCK_HEIGHT][BLOCK_WIDTH];
 int turn_block[BLOCK_HEIGHT][BLOCK_WIDTH];
 int stage[FIELD_HEIGHT][FIELD_WIDTH];
 int field[FIELD_HEIGHT][FIELD_WIDTH];
@@ -73,16 +78,28 @@ int clear_line_point[FIELD_HEIGHT - 3];
 
 int block_x;
 int block_y;
+int block_y_point;
 int collision_flag;
 int gameover_flag;
+int first_block_flag;
 int make_block_flag;
+int hold_block_flag;
 int clear_flag;
+int next_block_id;
 int block_id;
+int hold_block_id;
 int clear_count;
 int turn_point;
+int hold_turn_point;
+int score;
+int lv;
 
 int main(){
+
 	my_init_var();
+	my_make_field();
+	my_draw_field();
+	my_op();
 
 	while(gameover_flag == 0){
 		my_clear_field();
@@ -94,7 +111,8 @@ int main(){
 			my_make_field();
 			my_fix_block();
 			my_fall_block();
-		}else{
+		}
+		else{
 			my_clear_line();
 			my_make_field2();
 		}
@@ -103,11 +121,12 @@ int main(){
 		my_timer();
 	}
 
-	printf("gamever\n");
+	printf("gameover\n");
 		
 	return 0;
 }
 
+/*スコアボックス用に「壁」・文字の部分を追加*/
 void my_init_var(){
 	int i,j;
 
@@ -116,46 +135,213 @@ void my_init_var(){
 			stage[i][0] = 9;
 			stage[i][1] = 9;
 			stage[i][2] = 9;
+			stage[i][3] = 9;
+			stage[i][4] = 9;
+			stage[i][5] = 9;
+
+			stage[i][6] = 9;
+			stage[i][7] = 9;
+			stage[i][8] = 9;
+			
 			stage[20][j] = 9;
 			stage[21][j] = 9;
 			stage[22][j] = 9;
-			stage[i][15] = 9;
-			stage[i][16] = 9;
-			stage[i][17] = 9;		
+
+			stage[i][21] = 9;
+			stage[i][22] = 9;
+			stage[i][23] = 9;
+
+			stage[i][24] = 9;
+			stage[i][25] = 9;
+			stage[i][26] = 9;
+			stage[i][27] = 9;
+			stage[i][28] = 9;
+			stage[i][29] = 9;		
 		}
 	}
 
-	block_x = 7;
+	/*文字コード*/
+	/*HOLD*/
+	stage[1][1] = 72;
+	stage[1][2] = 79;
+	stage[1][3] = 76;
+	stage[1][4] = 68;
+
+	/*NEXT*/
+	stage[1][25] = 78;
+	stage[1][26] = 69;
+	stage[1][27] = 88;
+	stage[1][28] = 84;
+
+	/*SC*/
+	stage[8][25] = 0;
+	stage[8][26] = 0;
+	stage[8][27] = 83;
+	stage[8][28] = 67;
+	stage[9][25] = 0;
+	stage[9][26] = 0;
+	stage[9][27] = 0;
+	stage[9][28] = 0;
+
+	/*LV*/
+	stage[11][25] = 0;
+	stage[11][26] = 0;
+	stage[11][27] = 76;
+	stage[11][28] = 86;
+	stage[12][25] = 0;
+	stage[12][26] = 0;
+	stage[12][27] = 0;
+	stage[12][28] = 0;
+
+	block_x = 13;
 	block_y = 0;
+	block_y_point = 0;
 	collision_flag = 0;
 	gameover_flag = 0;
+	first_block_flag = 1;
 	make_block_flag = 1;
+	hold_block_flag = 0;
 	clear_flag = 0;
+	next_block_id = 0;
 	block_id = 0;
+	hold_block_id = 0;
 	clear_count = 0;
 	turn_point = 0;
+	hold_turn_point = 0;
+	score = 0;
+	lv = 1;
+
+	my_init_board();
 
 	srand((unsigned)time(NULL));
 }
 
+/*スコアボード作成用関数*/
+void my_init_board(){
+	int x,y;
+
+	for(y=0;y<BLOCK_HEIGHT;y++){
+		for(x=0;x<BLOCK_WIDTH;x++){
+			stage[y+2][x+25] = next_block[y][x];
+		}
+	}
+
+	for(y=0;y<BLOCK_HEIGHT;y++){
+		for(x=0;x<BLOCK_WIDTH;x++){
+			stage[y+2][x+1] = hold_block[y][x];
+		}
+	}
+
+	if(score < 10){
+		stage[9][28] = score;
+	}
+	else if(score < 100){
+		stage[9][27] = score / 10;
+		stage[9][28] = score - (score / 10) * 10;
+	}
+	else{
+		stage[9][27] = 9;
+		stage[9][28] = 9;
+	}
+	
+	if(lv < 3){
+		stage[12][28] = lv;
+	}
+	else{
+		stage[12][28] = 3;
+	}
+}
+
 void my_init_var2(){
-	block_x = 7;
+	block_x = 13;
 	block_y = 0;
+	block_y_point = 0;
 	make_block_flag = 1;
 	turn_point = 0;
 }
 
+/*オープニング*/
+void my_op(){
+	int any_key;
+
+	printf("BLOCK PUZZLE\n");
+	printf("press any key to start");
+	any_key = _getch();
+	if(any_key == 0 || any_key == 224)any_key = _getch();
+}
+
+/*ブロック生成を２段階に*/
+/*先に生成したブロックを「next_block_id」に保存*/
+/*すぐに「block_id」に保存しなおして再び*/
+/*ブロック生成処理を行い「next_block_id」に保存する*/
+/*２回目以降はこれを繰り返す*/
 void my_make_block(){
 	int x,y;
 
-	if(make_block_flag == 1){
-		block_id = (rand() % 6);
+	if(first_block_flag == 1){
+		next_block_id = (rand() % 6);
 		for(y=0;y<BLOCK_HEIGHT;y++){
 			for(x=0;x<BLOCK_WIDTH;x++){
-				block[y][x] = blocks[(block_id * BLOCK_HEIGHT) + y][x];
+				next_block[y][x] = blocks[(next_block_id * BLOCK_HEIGHT) + y][x];
+			}
+		}
+		first_block_flag = 0;
+	}	
+
+	if(make_block_flag == 1){
+		block_id = next_block_id;
+		next_block_id = (rand() % 6);
+		for(y=0;y<BLOCK_HEIGHT;y++){
+			for(x=0;x<BLOCK_WIDTH;x++){
+				block[y][x] = next_block[y][x];
+				next_block[y][x] = blocks[(next_block_id * BLOCK_HEIGHT) + y][x];
 			}
 		}
 		make_block_flag = 0;
+	}	
+}
+
+/*ブロックホールド処理*/
+/*初回ホールド時はそのまま「hold_block[4][4]」に*/
+/*その時の「block_id」など共に保存、初期化の「my_init_var2()」を行う*/
+/*２回目以降は先ほどの「hold_block[4][4]」との入れ替え処理を行い*/
+/*そのままブロックの位置だけを元に戻す*/
+void my_hold_block(){
+	int x,y;
+	
+	int tmp_block[4][4] = {0};
+	int tmp_block_id;
+	int tmp_turn_point;
+
+	if(hold_block_flag == 0){
+		for(y=0;y<BLOCK_HEIGHT;y++){
+			for(x=0;x<BLOCK_WIDTH;x++){
+				hold_block[y][x] = block[y][x];
+			}
+		}
+		hold_block_id = block_id;
+		hold_turn_point = turn_point;
+		my_init_var2();
+		my_make_block();
+		hold_block_flag = 1;
+	}
+	else{
+		for(y=0;y<BLOCK_HEIGHT;y++){
+			for(x=0;x<BLOCK_WIDTH;x++){
+				tmp_block[y][x] = block[y][x];
+				block[y][x] = hold_block[y][x];
+				hold_block[y][x] = tmp_block[y][x];
+			}
+		}
+		tmp_block_id = block_id;
+		tmp_turn_point = turn_point;
+		block_id = hold_block_id;
+		turn_point = hold_turn_point;
+		hold_block_id = tmp_block_id;
+		hold_turn_point = tmp_turn_point;
+
+		block_x = 13;
+		block_y = 0;
 	}
 }
 
@@ -169,40 +355,45 @@ void my_gameover(){
 
 void my_get_key(){
 	int key = 0;
-	
+
 	if(_kbhit()){
+			
 		key = _getch();
 		if(key == 0 || key == 224)key = _getch();
-		
+
 		switch(key){
-			case 0x4b:
-				my_collision_left();
-				if(collision_flag == 0){
-					block_x--;
-				}
-				break;
-			case 0x4d:
-				my_collision_right();
-				if(collision_flag == 0){
-					block_x++;
-				}
-				break;
-			case 0x50:
+		case 0x4b:
+			my_collision_left();
+			if(collision_flag == 0){
+				block_x--;
+			}
+			break;
+		case 0x4d:
+			my_collision_right();
+			if(collision_flag == 0){
+				block_x++;
+			}
+			break;
+		case 0x50:
+			my_collision_bottom();
+			while(collision_flag == 0){
+				block_y++;
 				my_collision_bottom();
-				while(collision_flag == 0){
-					block_y++;
-					my_collision_bottom();
-				}
-				break;
-			case 0x48:
-				my_turn_right();
-				break;
-			case 'z':
-				my_turn_left();
-				break;
-			default:
-				break;
+			}
+			break;
+		case 0x48:
+			my_turn_right();
+			break;
+		case 'z':
+			my_turn_left();
+			break;
+		case 'h':
+			my_hold_block();
+			break;
+		default:
+			break;
 		}
+
 	}
 }
 
@@ -210,10 +401,11 @@ void my_turn_right(){
 	int x,y;
 
 	turn_point++;
-
+	
 	for(y=0;y<BLOCK_HEIGHT;y++){
 		for(x=0;x<BLOCK_WIDTH;x++){
-			turn_block[y][x] = blocks[(block_id * BLOCK_HEIGHT) + y][(turn_point % 4 * BLOCK_WIDTH) + x];
+			turn_block[y][x] = 
+				blocks[(block_id * BLOCK_HEIGHT) + y][(turn_point % 4 * BLOCK_WIDTH) + x];
 		}
 	}
 
@@ -225,7 +417,8 @@ void my_turn_right(){
 				block[y][x] = turn_block[y][x];
 			}
 		}
-	}else{
+	}
+	else{
 		turn_point--;
 	}
 }
@@ -237,7 +430,8 @@ void my_turn_left(){
 
 	for(y=0;y<BLOCK_HEIGHT;y++){
 		for(x=0;x<BLOCK_WIDTH;x++){
-			turn_block[y][x] = blocks[(block_id * BLOCK_HEIGHT) + y][(turn_point % 4 * BLOCK_WIDTH) + x];
+			turn_block[y][x] = 
+				blocks[(block_id * BLOCK_HEIGHT) + y][(turn_point % 4 * BLOCK_WIDTH) + x];
 		}
 	}
 
@@ -249,16 +443,17 @@ void my_turn_left(){
 				block[y][x] = turn_block[y][x];
 			}
 		}
-	}else{
+	}
+	else{
 		turn_point -= 3;
 	}
 }
 
 void my_collision_left(){
 	int x,y;
-	
+
 	collision_flag = 0;
-	
+
 	for(y=0;y<BLOCK_HEIGHT;y++){
 		for(x=0;x<BLOCK_WIDTH;x++){
 			if(block[y][x] != 0){
@@ -272,9 +467,9 @@ void my_collision_left(){
 
 void my_collision_right(){
 	int x,y;
-	
+
 	collision_flag = 0;
-	
+
 	for(y=0;y<BLOCK_HEIGHT;y++){
 		for(x=0;x<BLOCK_WIDTH;x++){
 			if(block[y][x] != 0){
@@ -334,16 +529,25 @@ void my_collision_turn(){
 	}
 }
 
+/*ブロック固定を２段階に*/
+/*最初は固定せずにその時のブロックのy座標を保存*/
+/*次も同じy座標にブロックがあった場合は固定*/
 void my_fix_block(){
 	int x,y;
 
 	my_collision_bottom();
 
 	if(collision_flag != 0){
-		my_save_block();
-		my_search_line();
-		if(clear_flag == 0){
-			my_init_var2();
+		if(block_y_point == block_y){
+			my_save_block();
+			my_search_line();
+			if(clear_flag == 0){
+				my_init_var2();
+			}
+		}
+		else{
+			block_y_point = block_y;
+			block_y--;
 		}
 	}
 }
@@ -356,7 +560,7 @@ void my_search_line(){
 	}
 
 	for(i=0;i<FIELD_HEIGHT - 3;i++){
-		for(j=3;j<FIELD_WIDTH - 3;j++){
+		for(j=9;j<FIELD_WIDTH - 9;j++){
 			if(stage[i][j] == 0){
 				clear_line_point[i] = 1;
 				break;
@@ -367,7 +571,7 @@ void my_search_line(){
 	for(i=0;i<FIELD_HEIGHT - 3;i++){
 		if(clear_line_point[i] == 0){
 			clear_flag = 1;
-			break;
+			score++;
 		}
 	}
 }
@@ -380,14 +584,15 @@ void my_clear_line(){
 	if(clear_count < 2){
 		for(i=0;i<FIELD_HEIGHT - 3;i++){
 			if(clear_line_point[i] == 0){
-				for(j=3;j<FIELD_WIDTH - 3;j++){
+				for(j=9;j<FIELD_WIDTH - 9;j++){
 					stage[i][j] = 0;
 				}
 			}
 		}
 		clear_count++;
-	}else{
-		for(i=FIELD_HEIGHT - 4;i>=0;i--){
+	}
+	else{
+		for(i=FIELD_HEIGHT - 4;i >= 0;i--){
 			if(clear_line_point[i] != 0){
 				remain_line_point[remain_line_index] = i;
 				remain_line_index++;
@@ -395,8 +600,8 @@ void my_clear_line(){
 		}
 
 		remain_line_index = 0;
-		for(i=FIELD_HEIGHT - 4;i>=0;i--){
-			for(j=3;j<FIELD_WIDTH - 3;j++){
+		for(i=FIELD_HEIGHT - 4;i >= 0;i--){
+			for(j=9;j<FIELD_WIDTH - 9;j++){
 				stage[i][j] = stage[remain_line_point[remain_line_index]][j];
 			}
 			remain_line_index++;
@@ -405,11 +610,14 @@ void my_clear_line(){
 		clear_flag = 0;
 		clear_count = 0;
 		my_init_var2();
+		my_change_lv();
 	}
 }
 
 void my_make_field(){
 	int i,j,x,y;
+
+	my_init_board();
 
 	for(i=0;i<FIELD_HEIGHT;i++){
 		for(j=0;j<FIELD_WIDTH;j++){
@@ -427,9 +635,11 @@ void my_make_field(){
 void my_make_field2(){
 	int i,j;
 
+	my_init_board();
+
 	for(i=0;i<FIELD_HEIGHT;i++){
 		for(j=0;j<FIELD_WIDTH;j++){
-			field[i][j] = stage[i][j];
+			field[i][j] = stage[i][j];	
 		}
 	}
 }
@@ -444,14 +654,40 @@ void my_save_block(){
 	}
 }
 
+/*スコアボックスの表示をするべく*/
+/*文字コードの部分は「%c」で表示*/
 void my_draw_field(){
 	int i,j;
 
 	system("cls");
 
 	for(i=0;i<FIELD_HEIGHT - 2;i++){
-		for(j=2;j<FIELD_WIDTH - 2;j++){
+		for(j=0;j<6;j++){
+			if(field[i][j] >= 65)printf(" %c",field[i][j]);
+			else if(field[i][j] == 9)printf("■");
+			else if(field[i][j] == 1)printf("□");
+			else if(field[i][j] == 2)printf("■");
+			else printf("　");
+		}
+
+		for(j=8;j<FIELD_WIDTH - 8;j++){
 			if(field[i][j] == 9)printf("■");
+			else if(field[i][j] == 1)printf("□");
+			else if(field[i][j] == 2)printf("■");
+			else printf("　");
+		}
+
+		for(j=24;j<FIELD_WIDTH;j++){
+			if(i == 9 && j == 25)printf(" %d",field[i][j]);
+			else if(i == 9 && j == 26)printf(" %d",field[i][j]);
+			else if(i == 9 && j == 27)printf(" %d",field[i][j]);
+			else if(i == 9 && j == 28)printf(" %d",field[i][j]);
+			else if(i == 12 && j == 25)printf(" %d",field[i][j]);
+			else if(i == 12 && j == 26)printf(" %d",field[i][j]);
+			else if(i == 12 && j == 27)printf(" %d",field[i][j]);
+			else if(i == 12 && j == 28)printf(" %d",field[i][j]);
+			else if(field[i][j] >= 65)printf(" %c",field[i][j]);
+			else if(field[i][j] == 9)printf("■");
 			else if(field[i][j] == 1)printf("□");
 			else if(field[i][j] == 2)printf("■");
 			else printf("　");
@@ -478,9 +714,20 @@ void my_fall_block(){
 
 void my_timer(){
 	int x,y;
-	
-	for(y=0;y<10000;y++){
-		for(x=0;x<10000;x++){
+	for(y=0;y<20000 - (lv * 5000);y++){
+		for(x=0;x<20000 - (lv * 5000);x++){
 		}
+	}
+}
+
+void my_change_lv(){
+	if(score < 10){
+		lv = 1;
+	}
+	else if(score < 20){
+		lv = 2;
+	}
+	else{
+		lv = 3;
 	}
 }
